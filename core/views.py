2787a347edf8c451ca7bf1cca2b3a794f7c81f2a
nurulmasih <tobi4steve@gmail.com>
2022-03-17 23:43:47 +0100

    email verification

--- core/views.py
+++ core/views.py
@@ -6,6 +6,7 @@ from .models import Subscription
 from .serializers import SubscriptionModelSerializer
 from django.db import transaction
 from . import send_email as sm
+from validate_email import validate_email
 
 
 class SubscriptionViewSet(viewsets.ModelViewSet):
@@ -43,14 +44,26 @@ class SubscriptionViewSet(viewsets.ModelViewSet):
             )
         with transaction.atomic():
             subscription = serializer.save()
-            try:
-                sm.send_email(name=subscription.name, recepient=subscription.email)
-            except Exception as e:
+            is_valid = validate_email(subscription.email, verify=True)
+            if is_valid == True:
+                try:
+                    sm.send_email(name=subscription.name, recepient=subscription.email)
+                except Exception as e:
+                    return Response(
+                        {
+                            "success": False,
+                            "error": True,
+                            "msg": "Email failed to send. Please check your email and try again",
+                            "data": None,
+                        },
+                        status=status.HTTP_200_OK,
+                    )
+            else:
                 return Response(
                     {
                         "success": False,
                         "error": True,
-                        "msg": "Email failed to send. Please check your email and try again",
+                        "msg": "Email is Invalid",
                         "data": None,
                     },
                     status=status.HTTP_200_OK,
